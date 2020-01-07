3db0ed476ec669339c722948c569cf0efd5eff3b
OSCAR-Programming1 <programming@roswellrobotics.com>
2020-01-07 18:56:50 -0500

    Added Shooter Subsystem

--- src/main/java/frc/team832/robot/Constants.java
+++ src/main/java/frc/team832/robot/Constants.java
@@ -1,8 +1,29 @@
 package frc.team832.robot;
 
 public class Constants {
+
+    //drivetrain
+
+
+    //shooter
+    public static double SHOOTER_SPIN_UP_kP = 0;
+    public static double SHOOTER_SPIN_UP_kD = 0;
+    public static double SHOOTER_SPIN_UP_kF = 0;
+
+    public static double SHOOTING_kP = 0;
+    public static double SHOOTING_kD = 0;
+    public static double SHOOTING_kF = 0;
+
+    public static double SHOOTER_SPIN_DOWN_kP = 0;
+    public static double SHOOTER_SPIN_DOWN_kD = 0;
+    public static double SHOOTER_SPIN_DOWN_kF = 0;
+
+    //CAN IDs
     public static final int RIGHT_MASTER_CAN_ID = 1;
     public static final int LEFT_MASTER_CAN_ID = 2;
     public static final int RIGHT_SLAVE_CAN_ID = 3;
     public static final int LEFT_SLAVE_CAN_ID = 4;
+    public static final int SHOOTER_ID_PRIMARY = 5;
+    public static final int SHOOTER_ID_SECONDARY = 6;
+
 }
--- src/main/java/frc/team832/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team832/robot/subsystems/Drivetrain.java
@@ -1,9 +1,8 @@
 package frc.team832.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.team832.lib.motorcontrol2.vendor.CANTalonFX;
 import frc.team832.robot.Constants;
+import frc.team832.lib.motorcontrol2.vendor.CANTalonFX;
 import frc.team832.robot.commands.TemplateCommand;
 
 public class Drivetrain extends SubsystemBase {
@@ -12,7 +11,7 @@ public class Drivetrain extends SubsystemBase {
     private CANTalonFX rightMaster, rightSlave, leftMaster, leftSlave;
 
 
-   public Drivetrain() {
+    public Drivetrain() {
         leftMaster = new CANTalonFX(Constants.LEFT_MASTER_CAN_ID);
         leftSlave = new CANTalonFX(Constants.LEFT_SLAVE_CAN_ID);
         rightMaster = new CANTalonFX(Constants.RIGHT_MASTER_CAN_ID);
@@ -36,13 +35,13 @@ public class Drivetrain extends SubsystemBase {
         setDefaultCommand(new TemplateCommand(this));
 
         initSuccessful = true;
-   }
+    }
 
-   public void setCurrentLimit(int amps) {
-       leftMaster.limitInputCurrent(amps);
-       leftSlave.limitInputCurrent(amps);
-       rightMaster.limitInputCurrent(amps);
-       rightSlave.limitInputCurrent(amps);
-   }
+    public void setCurrentLimit(int amps) {
+        leftMaster.limitInputCurrent(amps);
+        leftSlave.limitInputCurrent(amps);
+        rightMaster.limitInputCurrent(amps);
+        rightSlave.limitInputCurrent(amps);
+    }
 
 }
